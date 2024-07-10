3e1de38544f64095e4eff4ff17223d3fc593204a
Joma <15043788+CryptoJoma@users.noreply.github.com>
2024-07-10 07:19:57 -0600

    Delete backend/api/update_minutes.php

--- backend/api/update_minutes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include '../archives/functions.php';
-
-// Call the function to update minutes
-minutes_checker();
-echo "Minutes balance have been updated successfully for all the users.";
-?>
